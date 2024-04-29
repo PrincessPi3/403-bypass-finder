1146e3f799d93ed4ce5317db1fcb8e509af99479
PrincessPi3 <human.bagel@gmail.com>
2024-04-28 20:51:55 -0600

    upgraded and cleaned up

--- 403-bypass-finder.sh
+++ 403-bypass-finder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffuf -w 403-bypass-start.txt:START -w 403-bypass-end.txt:END -u "$1/START$2END" -ac -o results-`date "+%A-%d-%b-%Y-%I:%M:%S-%p"`.html -of html -mc all -fc 403,404 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+ffuf -w assets/403-bypass-start.txt:START -w assets/403-bypass-end.txt:END -u "$1/START$2END" -o results/results-`date "+%A-%d-%b-%Y-%I:%M:%S-%p"`.html -of html -mc all -fc 403,404 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' -v -r
